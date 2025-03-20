e9a3d38577bce0156ae4ebd3783b367830b3f9c0
MESTUSA <menahil1006@gmail.com>
2025-03-19 21:55:37 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,23 @@
-// vite.config.ts
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { componentTagger } from "lovable-tagger";
 
-export default defineConfig({
-  base: '/self-healing-data-symphony/',
-  plugins: [react()],
-})
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => ({
+base: '/self-healing-data-symphony/',  
+server: {
+    host: "::",
+    port: 8080,
+  },
+  plugins: [
+    react(),
+    mode === 'development' &&
+    componentTagger(),
+  ].filter(Boolean),
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+}));
